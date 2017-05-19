ae9f03d8ad5f99eb031c9e3181f69153f1c6c70c
rezaisrad <rezarad77@gmail.com>
2017-05-19 16:37:17 -0400

    cryptocurrency.py v1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Historical Data:
 https://www.quandl.com/tools/python
+
+API:
+https://www.cryptocompare.com/coins/#/btc
--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,29 +1,37 @@
 import os, path
 import requests
-import sqlite3
-import json
+import pandas as pd
+import time
+import datetime as datetime
 
-# get list of avaiable cryptocurrencies
-response = requests.get('https://www.cryptocompare.com/api/data/coinlist/')
-response_json = response.json()
-currency_list = response_json['Data'].keys()
-currency_list = [currency.encode('utf8') for currency in currency_list]
 
-# maximum range of currency prices is [0:22]
-currency_top = ["BTC ", "ETH", "XRP", "XEM", "LTC", "XLM", "ETC", "BCN", "DASH",
+df = pd.DataFrame({}, columns = ['LASTUPDATE', 'HIGH24HOUR',  'LASTVOLUMETO',
+                                                            'MKTCAP', 'LASTVOLUME', 'PRICE', 'SUPPLY', 'CHANGEPCT24HOUR',
+                                                            'LOW24HOUR', 'OPEN24HOUR', 'VOLUME24HOURTO', 'FLAGS',
+                                                            'VOLUME24HOUR', 'CHANGE24HOUR', 'TYPE', 'LASTTRADEID',
+                                                            'FROMSYMBOL', 'LASTMARKET', 'MARKET', 'TOSYMBOL'])
+
+# maximum size of API query for currency prices is 23
+currency_top = ["BTC", "ETH", "XRP", "XEM", "LTC", "XLM", "ETC", "BCN", "DASH",
                             "DGB", "XMR", "SC", "DOGE", "BTS", "GNT", "ARDR", "EMC2", "ZEC",
                             "STRAT", "NXT", "STEEM", "RDD", "GNO"]
 currency_str = ','.join(currency_top)
-
 parameters  = {'fsyms': currency_str, 'tsyms': 'USD'}
-response2 = requests.get('https://min-api.cryptocompare.com/data/pricemultifull', params=parameters)
 
-response2.json()['RAW']['XLM']['USD'].keys()
+while True:
+    response = requests.get('https://www.cryptocompare.com/api/data/coinlist/')
+    response_json = response.json()
+    currency_list = response_json['Data'].keys()
+    currency_list = [currency.encode('utf8') for currency in currency_list]
 
-conn = sqlite3.connect('cryptcoin.db')
-cursor = conn.cursor()
-cursor.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-                    .format(tn=table_name, cn=new_column1, ct=column_type))
+    response = requests.get('https://min-api.cryptocompare.com/data/pricemultifull', params=parameters)
+    crypt_dict = [response.json()['RAW'][currency]['USD'] for currency in currency_top]
 
-conn.commit()
-conn.close()
+    for currency in currency_top:
+        crypt = response.json()['RAW'][currency]['USD']
+        df = df.append(crypt, ignore_index=True)
+        time.sleep(10)
+
+    if df.size > 10000:
+        df.write_csv("./data/cryptcoin_{0}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")))
+        df = {}
