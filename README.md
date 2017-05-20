48bd7430eeb6df7b2ae1d89a6d8186c3e833d855
rezaisrad <rezarad77@gmail.com>
2017-05-20 11:58:44 -0400

    fixed loops and updated stdout

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ https://www.quandl.com/tools/python
 
 API:
 https://www.cryptocompare.com/coins/#/btc
+
+Notes:
+"We also observed that Random Forests gave us a better accuracy rate as compared to GLM.
+This is because RFs use nonparametric decision trees, so outliers and linear separability of the data are not concerns." http://cs229.stanford.edu/proj2014/Isaac%20Madan,%20Shaurya%20Saluja,%20Aojia%20Zhao,Automated%20Bitcoin%20Trading%20via%20Machine%20Learning%20Algorithms.pdf)
--- cryptocurrency.py
+++ cryptocurrency.py
@@ -4,35 +4,33 @@ import pandas as pd
 import time
 import datetime as datetime
 
-
 # maximum size of API query for currency prices is 23
 currency_top = ["BTC", "ETH", "XRP", "XEM", "LTC", "XLM", "ETC", "BCN", "DASH",
                             "DGB", "XMR", "SC", "DOGE", "BTS", "GNT", "ARDR", "EMC2", "ZEC",
                             "STRAT", "NXT", "STEEM", "RDD", "GNO"]
+                            
+columns = ['LASTUPDATE', 'HIGH24HOUR',  'LASTVOLUMETO',
+                                'MKTCAP', 'LASTVOLUME', 'PRICE', 'SUPPLY', 'CHANGEPCT24HOUR',
+                                'LOW24HOUR', 'OPEN24HOUR', 'VOLUME24HOURTO', 'FLAGS',
+                                'VOLUME24HOUR', 'CHANGE24HOUR', 'TYPE', 'LASTTRADEID',
+                                'FROMSYMBOL', 'LASTMARKET', 'MARKET', 'TOSYMBOL']
+df = pd.DataFrame({}, columns=columns)
+
 currency_str = ','.join(currency_top)
 parameters  = {'fsyms': currency_str, 'tsyms': 'USD'}
 
-while True:
-    response = requests.get('https://www.cryptocompare.com/api/data/coinlist/')
-    response_json = response.json()
-    currency_list = response_json['Data'].keys()
-    currency_list = [currency.encode('utf8') for currency in currency_list]
 
+while True:
     response = requests.get('https://min-api.cryptocompare.com/data/pricemultifull', params=parameters)
-    crypt_dict = [response.json()['RAW'][currency]['USD'] for currency in currency_top]
-    
-    df = pd.DataFrame({}, columns = ['LASTUPDATE', 'HIGH24HOUR',  'LASTVOLUMETO',
-                                    'MKTCAP', 'LASTVOLUME', 'PRICE', 'SUPPLY', 'CHANGEPCT24HOUR',
-                                    'LOW24HOUR', 'OPEN24HOUR', 'VOLUME24HOURTO', 'FLAGS',
-                                    'VOLUME24HOUR', 'CHANGE24HOUR', 'TYPE', 'LASTTRADEID',
-                                    'FROMSYMBOL', 'LASTMARKET', 'MARKET', 'TOSYMBOL'])
 
     for currency in currency_top:
         crypt = response.json()['RAW'][currency]['USD']
         df = df.append(crypt, ignore_index=True)
-        time.sleep(10)
-        print "number of observations in current df: {0}".format(df.size)
+    print df.loc[:, ['FROMSYMBOL', 'TOSYMBOL', 'PRICE', 'MARKET', 'CHANGEPCT24HOUR']].tail(23)
+    print "number of observations in current df: {0}".format(len(df))
+    time.sleep(10)
 
-    if df.size > 10000:
+    if len(df) > 10000:
         print "new csv file outputted"
         df.to_csv("cryptcoin_{0}.csv".format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")))
+        df = pd.DataFrame({}, columns=columns)
